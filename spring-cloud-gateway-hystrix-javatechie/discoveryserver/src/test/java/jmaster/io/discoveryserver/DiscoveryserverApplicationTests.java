64ee2a7a725c82a1b9b63c561b1c664cfb9c6d3c
PhamNgocThuong <ngocthuong2561992@gmail.com>
2022-11-28 18:14:47 +0700

    discovery server

---
+++ spring-cloud-gateway-hystrix-javatechie/discoveryserver/src/test/java/jmaster/io/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package jmaster.io.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
